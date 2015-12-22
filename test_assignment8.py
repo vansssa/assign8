76bdc526b51c9abc1bb5b3da9c0b1b6745d4c003
vansssa <sqa-pt@KKBOXdeMacBook-Air-80.local>
2015-12-22 18:07:53 +0800

    edit unittest : using setup

--- test_assignment8.py
+++ test_assignment8.py
@@ -2,8 +2,14 @@ import unittest
 from assignment8 import fib
 
 class TestAssignment8(unittest.TestCase):
+	def setUp(self) :
+		self.myfib=fib()
+
+
 	def test_fib_func(self) :
-		self.assertEqual(fib().next() , 1)
+		solution_list=[1,1,2,3,5,8,13,21,34]
+		for i in solution_list :
+			self.assertEqual(self.myfib.next() , i)
 
 
 if __name__ == '__main__':
